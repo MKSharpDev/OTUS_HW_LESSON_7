4386c14c5f902d5bf49fc48b12be448a1352588b
M K <kuzz.ma@yandex.ru>
2023-09-14 19:11:11 +0300

    АПИ готово

---
+++ WebApi/AppDbContext.cs
@@ -0,0 +1,18 @@
+﻿using WebApi;
+
+using Microsoft.EntityFrameworkCore;
+using WebApi.Models;
+
+namespace WebApi
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+          : base(options)
+        {
+        }
+
+        
+        public DbSet<Customer> Customer { get; set; }
+    }
+}
--- WebApi/Controllers/CustomerController.cs
+++ WebApi/Controllers/CustomerController.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -10,33 +11,56 @@ namespace WebApi.Controllers
     [Route("api/[Controller]")]
     public class CustomerController : Controller
     {
+        private readonly AppDbContext _context;
+        private readonly ILogger<CustomerController> _logger;
+        public CustomerController(AppDbContext context, ILogger<CustomerController> logger)
+        {
+            _context = context;
+            _logger = logger;
+        }
 
 
         [HttpGet()]   
-        public Task<Customer> GetCustomerAsync()
+        public IEnumerable<Customer> GetCustomerAsync()
         {
-            throw new NotImplementedException();
+            return this._context.Customer.ToArray();
         }
+
         [HttpGet("{id}")]
-        public Task<Customer> GetCustomerByIdAsync([FromRoute] long id)
+        public IActionResult Get(int id)
         {
-            throw new NotImplementedException();
+            var customer = _context.Customer.SingleOrDefault(p => p.Id == id);
+            if (customer != null)
+            {
+                return Ok(customer);
+            }
+
+            return NotFound();
         }
 
-        [HttpPost]   
-        public Task<long> CreateCustomerAsync([FromBody] Customer customer)
-        {
-            throw new NotImplementedException();
-        }
-
-
-        [HttpPut]
+        [HttpPost]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        [ProducesResponseType(StatusCodes.Status409Conflict)]
-        public Task<long> PutCustomerAsync([FromBody] Customer customer)
+        public async Task<ActionResult<Customer>> Post([FromBody] Customer customer)
         {
-            throw new NotImplementedException();
+            var SearchCustomer = _context.Customer.SingleOrDefault(p => p.Id == customer.Id);
+            if (SearchCustomer == null)
+            {
+                this._context.Customer.Add(customer);
+                await this._context.SaveChangesAsync();
+                return CreatedAtAction(nameof(Post), new { id = customer.Id }, customer);
+            }
+
+            var error = new ProblemDetails
+            {
+                Type = "https://httpstatuses.com/409",
+                Status = StatusCodes.Status409Conflict,
+                Title = "Already exist"
+            };
+            return StatusCode(409, error); 
+
+            
+
         }
     }
 }
